aba7a63b5c83c89550e5ecc14a04ae34404fc525
surendar77 <surenc1690@gmail.com>
2026-09-21 14:24:47 +0530

    third commit

--- routes/emp_route.js
+++ routes/emp_route.js
@@ -1,20 +1,54 @@
 let express=require('express');
 let router=express.Router();
+let bcrypt=require('bcrypt');
 let {users}=require('../models/users');
 //localhost:3000/api/emp/register
 router.post("/register",async (req,res)=>{
     console.log(req.body);
+    req.body.password=await bcrypt.hash(req.body.password,10);
     let newuser=users(req.body);
     let result= await newuser.save();
     res.send(result);
 })
-router.post("/login",(req,res)=>{
-    res.send("login page called");
+
+router.post("/login",async (req,res)=>{
+    let result=await users.findOne({email:req.body.email})
+    if(result){
+let matchpass=await bcrypt.compare(req.body.password,result.password);
+    if(matchpass){
+        res.send("login successfull");
+    }else{
+        res.send("login failed");
+    }
+    }else{
+        res.send("user not found");
+    }
 })
+
+
+
+
 router.get("/view task",(req,res)=>{
     res.send("view task page called");
 })
 router.put("/updatestatus",(req,res)=>{
     res.send("update status page called");
 })
+
+router.patch("/updateprofile/:id",async (req,res)=>{
+  let data=req.body;
+  if(data.password){
+    data.password=await bcrypt.hash(data.password,10);
+  } 
+  let result=await users.findByIdAndUpdate(req.params.id,data,{new:true});
+  res.send(result);
+});
+
+
+
+
+
+
+
+
 module.exports=router;
--- routes/hr_route.js
+++ routes/hr_route.js
@@ -1,11 +1,29 @@
 let express=require('express');
 let router=express.Router();
-router.get("/viewemployees",(req,res)=>{
-    res.send("View employees route");
+let {users} =require('../models/users');
+router.get("/viewemployees",async (req,res)=>{
+    let result=await  users.find();
+    res.send(result);
 });
 router.post("/assign-task",(req,res)=>{
     res.send("assign task route called");
 })
-// localhost:3000/api/hr/viewtasks => GET
-// localhost:3000/api/hr/deleteEmp => Delete
+
+
+
+router.delete("/deleteemployee/:id",async (req,res)=>{
+    let result=await users.findByIdAndDelete(req.params.id)
+    if(result){
+        res.send("employee deleted success");
+    }else{
+        res.send("no user found");
+    }
+})
+//in postman => choose delete method
+//localhost:3000/api/hr/deleteemployee/6ab0d4278b577c7452bd4246
+
+
+
+
+
 module.exports=router;
